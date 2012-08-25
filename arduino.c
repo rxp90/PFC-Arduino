a7864364f25e83cf5881e35c5764d2bc54cda042
rxp90 <raul.ruloxp@gmail.com>
2012-08-25 22:26:26 +0300

    Update arduino.c

--- arduino.c
+++ arduino.c
@@ -8,10 +8,10 @@
 #define direccionReceptor 0x2
 
 boolean accelSleep;
-char inData[20]; // Allocate some space for the string
+char inData[30]; // Allocate some space for the string
 char inChar; // Where to store the character read
 byte index = 0; // Index into array; where to store the character
-String cadenaIdentificacion = String(direccionMando) + "," + String(idRed) + "," + String(canal) + "," + String(direccionReceptor);
+String cadenaIdentificacion = "MY="String(direccionMando) + ",ID" + String(idRed) + ",CH" + String(canal) + ",DL" + String(direccionReceptor);
 void setup()
 {  
   // Configuración puerto Serie
@@ -30,7 +30,7 @@ char Comp(String This) {
   while (Serial.available() > 0) // Don't read unless
     // there you know there is data
   {
-    if(index < 19) // One less than the size of the array
+    if(index < 29) // One less than the size of the array
     {
       inChar = Serial.read(); // Read a character
       inData[index] = inChar; // Store it
@@ -40,7 +40,7 @@ char Comp(String This) {
   }
 
   if ((String(inData).compareTo(This))  == 0) {
-    for (int i=0;i<19;i++) {
+    for (int i=0;i<29;i++) {
       inData[i]=0;
     }
     index=0;
